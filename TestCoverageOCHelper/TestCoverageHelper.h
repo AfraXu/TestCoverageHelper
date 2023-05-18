80f20fe189eb42fa5a74f69f4d089dfdfe3452c6
徐芙蓉 <1181228489@qq.com>
2023-05-18 09:26:44 +0800

    用于收集代码覆盖率的framework

---
+++ TestCoverageOCHelper/TestCoverageHelper.h
@@ -0,0 +1,19 @@
+//
+//  TestCoverageHelper.h
+//  TestCoverageOCHelper
+//
+//  Created by 徐芙蓉 on 2023/5/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestCoverageHelper : NSObject
++ (instancetype)shareInstance;
+- (void)resignInfo;
+- (void)clearCoverage;
+- (void)getCoverage;
+@end
+
+NS_ASSUME_NONNULL_END
